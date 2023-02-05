5776063175df9e491766e099598a140fe59a4425
Vandame <vandamedeveloper@gmail.com>
2023-02-05 15:57:20 +0100

    feat(auth): added routing

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -2,16 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
-
-
+import { AuthenticationRoutingModule } from './authentication-routing.module';
 
 @NgModule({
-  declarations: [
-    LoginComponent,
-    SignupComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [LoginComponent, SignupComponent],
+  imports: [CommonModule, AuthenticationRoutingModule],
 })
-export class AuthenticationModule { }
+export class AuthenticationModule {}

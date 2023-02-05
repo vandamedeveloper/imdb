bbbdd272db645407c1361ecc0626256e724ebea0
vandamedeveloper <99797005+vandamedeveloper@users.noreply.github.com>
2023-02-05 15:58:47 +0100

    Feat/auth (#2)
    * feat(auth): added store folder and login + auth components
    
    * feat(auth): added routing

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { AuthenticationRoutingModule } from './authentication-routing.module';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [LoginComponent, SignupComponent],
+  imports: [CommonModule, AuthenticationRoutingModule],
 })
-export class AuthenticationModule { }
+export class AuthenticationModule {}

1fd8d41b191f382cce397f74dec28d5d6cba9e8e
Vandame <vandamedeveloper@gmail.com>
2023-02-05 18:56:07 +0100

    feat(movies): added main components and store folder

--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MoviesContainerComponent } from './movies-container/movies-container.component';
+import { MovieComponent } from './movie/movie.component';
+import { MoviesListComponent } from './movies-list/movies-list.component';
+import { MoviesSearchComponent } from './movies-search/movies-search.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MoviesContainerComponent,
+    MovieComponent,
+    MoviesListComponent,
+    MoviesSearchComponent
+  ],
   imports: [
     CommonModule
   ]

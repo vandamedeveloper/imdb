8ef06e486df16cd15e46e907adadbd873aaa24b7
vandamedeveloper <99797005+vandamedeveloper@users.noreply.github.com>
2023-02-05 19:03:11 +0100

    feat(movies): added main components and store folder (#5)

---
+++ src/app/movies/movies-search/movies-search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movies-search',
+  templateUrl: './movies-search.component.html',
+  styleUrls: ['./movies-search.component.scss']
+})
+export class MoviesSearchComponent {
+
+}

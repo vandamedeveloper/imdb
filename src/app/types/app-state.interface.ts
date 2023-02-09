9232d4242ede4aa5d4406cb333ff8a613c80505e
vandamedeveloper <99797005+vandamedeveloper@users.noreply.github.com>
2023-02-09 16:58:47 +0100

    Revert "Feat/auth (#14)" (#15)
    This reverts commit 1537e0757704e8124ff2652b41461789be5feb93.

--- src/app/types/app-state.interface.ts
+++ src/app/types/app-state.interface.ts
@@ -1,7 +1,5 @@
-import { AuthState } from '../authentication/store/auth-state.interface';
 import { MovieState } from '../movies/store/movie-state.interface';
 
 export interface AppState {
   movies: MovieState;
-  auth: AuthState;
 }

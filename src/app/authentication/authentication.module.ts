7412815c97272755aae5b1004ab458bef67d8439
Vandame <vandamedeveloper@gmail.com>
2023-02-09 12:53:02 +0100

    feat(auth): added login and signup screens with angular material

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -3,9 +3,24 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [LoginComponent, SignupComponent],
-  imports: [CommonModule, AuthenticationRoutingModule],
+  imports: [
+    CommonModule,
+    AuthenticationRoutingModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
 })
 export class AuthenticationModule {}
--- src/app/authentication/components/signup/signup.component.ts
+++ src/app/authentication/components/signup/signup.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent {
+  loginForm: FormGroup;
+  hide = true;
 
+  constructor(private _fBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this._initializeForm();
+  }
+
+  _initializeForm() {
+    this.loginForm = this._fBuilder.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSignup() {}
 }

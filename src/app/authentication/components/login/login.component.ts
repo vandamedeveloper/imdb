68019efc97aa0c3a15829f77cb3c388c7eb94e8d
Vandame <vandamedeveloper@gmail.com>
2023-02-09 12:53:02 +0100

    feat(auth): added login and signup screens with angular material

--- src/app/authentication/components/login/login.component.ts
+++ src/app/authentication/components/login/login.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  hide = true;
 
+  constructor(private _fBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this._initializeForm();
+  }
+
+  _initializeForm() {
+    this.loginForm = this._fBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  onLogin() {}
 }
